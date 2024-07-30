0258a884bb00beeb564438723e3951bc37b1c143
Bilal <bilalsagheer13@gmail.com>
2024-07-30 15:56:46 +0500

    update logo

--- resources/js/Components/Logos/ApplicationLogo.vue
+++ resources/js/Components/Logos/ApplicationLogo.vue
@@ -4,6 +4,6 @@ import { router } from "@inertiajs/vue3";
 
 <template>
     <Link :href="route('home')" class="myPrimaryParagraph">
-        <img class="h-6" src="/logo/logo-myissue.svg" alt="Logo" />
+        <img class="h-6" src="/logo/logo-b.jpg" alt="Logo" />
     </Link>
 </template>
